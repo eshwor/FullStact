8baad15c5704fd1616d51fb1231329880ae16ad6
Ishwor <ishworkhatri26@yahoo.com>
2019-03-03 08:38:40 -0500

    ProjectList Cleanup

---
+++ src/components/project/ProjectSummary.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+  return(
+      <div className="card z-depth-0 project-summary">
+
+        <div className="card-content grey-text darken-3">
+          <span className="card-title">Project Title</span>
+          <p> Posted by the Ishwor</p>
+          <p className="gray-text">1st January, 1PM</p>
+        </div>
+
+      </div>
+  )
+}
+
+export default ProjectSummary;
