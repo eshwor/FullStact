3c81dc898fbee189e085d7095b7ad78edfd13b91
Ishwor <ishworkhatri26@yahoo.com>
2019-03-03 12:26:33 -0500

    Almost complete the setting of page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/project/ProjectDetails';
+import SignIn from './components/auth/Signin';
+import SignUp from './components/auth/Signup';
+import CreateProject from './components/project/CreateProject';
+
 
 class App extends Component {
   render() {
@@ -16,6 +20,9 @@ class App extends Component {
           <Switch>
             <Route exact path='/' component={Dashboard} />
             <Route path='/project/:id' component={ProjectDetails} />
+            <Route path='/signin' component={SignIn} />
+            <Route path='/signup' component={SignUp} />
+            <Route path='/create' component={CreateProject} />
           </Switch>
         </div>
       </BrowserRouter>
