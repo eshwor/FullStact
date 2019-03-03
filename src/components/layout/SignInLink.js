eb3b0b19ea8cb1f6241fcfdfd13dffb42634fa43
Ishwor <ishworkhatri26@yahoo.com>
2019-03-03 12:40:08 -0500

    Finishing up all the basic settings

--- src/components/layout/SignInLink.js
+++ src/components/layout/SignInLink.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const SignInLink = () => {
   return (
     <ul className="right">
-      <li><NavLink to='/'> New Project </NavLink></li>
+      <li><NavLink to='/create'> New Project </NavLink></li>
       <li><NavLink to='/'> Log Out </NavLink></li>
       <li><NavLink to='/' className="btn btn-floating black lighten-1">FS</NavLink></li>
     </ul>
