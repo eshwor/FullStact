cbbea54958c99ff7e1131d503c092c07370f547c
Ishwor <ishworkhatri26@yahoo.com>
2019-03-05 06:13:04 -0500

    Setting up Reducer and Testing it with dummy data

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,8 +1,14 @@
 
-const initState = {};
-const projectReducer = (state = initState, action) => {
-  return state;
+const initState = {
+  projects: [
+    {id: 1, title: 'help me find peach', content: 'blah blah blah'},
+    {id: 2, title: 'help me find star', content: 'blah blah blah'},
+    {id: 3, title: 'help me find apple', content: 'blah blah blah'}
+  ]
+};
 
+const projectReducer = (state = initState, action) => {
+  return state
 }
 
 export default projectReducer;
